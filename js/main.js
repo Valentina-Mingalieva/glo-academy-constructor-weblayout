12df92f3f9457877ac6a38c76bb47edca2039a2c
Valentina Mingalieva <valentina.mingalieva@gmail.com>
2021-07-20 20:23:59 +0200

    day one

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* 
+
 new Swiper('.swiper-container', {
 	loop: true,
 	navigation: {
@@ -20,4 +22,105 @@ const menu = document.querySelector('.header');
 menuButton.addEventListener('click', function () {
 	menuButton.classList.toggle('menu-button-active');
 	menu.classList.toggle('header-active');
-})
+}) 
+
+*/
+
+const getElement = (tagName, classNames, attributes) => {
+	const element = document.createElement(tagName);
+
+	if (classNames) {
+		element.classList.add(...classNames);
+	}
+
+	if (attributes) {
+		for (const attribute in attributes) {
+			element[attribute] = attributes[attribute];
+		}
+	}
+
+	return element;
+}
+
+const createHeader = (param) => {
+	const header = getElement('header');
+	const container = getElement('div', ['container']);
+	const wrapper = getElement('div', ['header']);
+	
+
+	if (param.header.logo) {
+		const logo = getElement('img', ['logo'], {
+			src: param.header.logo,
+			alt: 'Logo' + param.title,
+		});
+		wrapper.append(logo);
+	}
+
+	if (param.header.social) {
+		const socialWrapper = getElement('div', ['social']);
+		const allSocial = param.header.social.map(item => {
+			const socialLink = getElement('a', ['social-link']);
+			socialLink.append(getElement('img', [], {
+				src: item.image,
+				alt: item.title,
+			}))
+			socialLink.href = item.link;
+
+			return socialLink;
+		})
+		socialWrapper.append(...allSocial);
+		wrapper.append(socialWrapper);
+	}
+
+	if (param.header.menu) {
+		
+	}
+
+	header.append(container);
+	container.append(wrapper);
+
+	return header;
+}
+
+const movieConstructor = (selector, options) => {
+	const app = document.querySelector(selector);
+	app.classList.add('body-app');
+
+	if (options.header) {
+		app.append(createHeader(options));
+	}
+}
+
+movieConstructor('.app', {
+	title: 'WITCH',
+	header: {
+		logo: 'witcher/logo.png',
+		social: [{
+			title: 'Twitter',
+			link: 'http://twitter.com', 
+			image: 'witcher/social/twitter.svg',
+		}, {
+			title: 'Instagram',
+			link: 'http://instagram.com', 
+			image: 'witcher/social/instagram.svg',
+		}, {
+			title: 'Facebook',
+			link: 'http://facebook.com', 
+			image: 'witcher/social/facebook.svg',
+			}],
+		menu: [
+			{
+				title: 'Описание',
+				link: '#',
+			},
+			{
+				title: 'Трейлер',
+				link: '#',
+			},
+			{
+				title: 'Отзывы',
+				link: '#',
+			},
+	]
+	}
+});
